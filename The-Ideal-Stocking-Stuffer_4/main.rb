dfac311dfaf72d5c162cd64b5cf3ba4ab23c1d01
jakenotjacob <jake.campbell91@gmail.com>
2015-12-16 02:49:49 -0700

    Finish problem 4, part 2

--- The-Ideal-Stocking-Stuffer_4/main.rb
+++ The-Ideal-Stocking-Stuffer_4/main.rb
@@ -5,7 +5,7 @@ lowest = 0
 found = false
 until found
   x = Digest::MD5.hexdigest("bgvyzdsv#{lowest}")
-  if x[0..4] == "000000"
+  if x[0..5] == "000000"
     found = true
   else
     lowest += 1
