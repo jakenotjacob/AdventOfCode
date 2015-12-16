a9c144e4b2447d736d061e3bfe76768a008f0d2c
jakenotjacob <jake.campbell91@gmail.com>
2015-12-16 02:20:06 -0700

    Finish problem 3, part 2

--- Perfectly-Spherical-Houses-in-a-Vacuum_3/main.rb
+++ Perfectly-Spherical-Houses-in-a-Vacuum_3/main.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-input = File.open("input.txt")
+input = File.open("input.txt").readlines.first.chomp.scan(/./)
 
 def plant(grid, x, y)
   new_x = "#{x}"
@@ -14,20 +14,29 @@ end
 
 
 grid = {"0"=> {"0"=> true}}
-x=0
-y=0
-while direction = input.getc
-  case direction
-  when "^"
-    y+=1
-  when "v"
-    y-=1
-  when ">"
-    x+=1
-  when "<"
-    x-=1
+santa_1 = []
+santa_2 = []
+while c = input.shift
+  santa_1 << c
+  santa_2 << input.shift
+end
+[santa_1, santa_2].each do |s|
+  x = 0
+  y = 0
+  while direction = s.shift
+    case direction
+    when "^"
+      y+=1
+    when "v"
+      y-=1
+    when ">"
+      x+=1
+    when "<"
+      x-=1
+    end
+    plant(grid, x, y)
   end
-  plant(grid, x, y)
 end
 
+puts grid
 puts grid.map{|k,v|v.flatten}.flatten.count(true)
