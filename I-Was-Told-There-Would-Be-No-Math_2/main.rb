47c7c3d4533ef3be8ea41e7fe128c3e2356cd737
jakenotjacob <jake.campbell91@gmail.com>
2015-12-16 00:26:01 -0700

    Finish problem 2, part 2

--- I-Was-Told-There-Would-Be-No-Math_2/main.rb
+++ I-Was-Told-There-Would-Be-No-Math_2/main.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/env ruby
 
-boxes = File.readlines("input.txt").map(&:chomp)
+boxes = File.readlines("input.txt").map(&:chomp).map{|b|b.split("x").map(&:to_i)}
 
-total_sq_paper = 
-  boxes.map!{ |box|
-    l,w,h = box.split("x").map(&:to_i)
-    surface_area = 2*(l*(w+h)+(w*h))
-    small_side = [l,w,h].min(2).inject(&:*)
-    surface_area + small_side
-  }.inject(&:+)
+total_sq_paper = boxes.map{ |box|
+                   l,w,h = box
+                   surface_area = 2*(l*(w+h)+(w*h))
+                   small_side = [l,w,h].min(2).inject(&:*)
+                   surface_area + small_side
+                 }.inject(&:+)
 
+total_ribbon = boxes.map{ |box|
+  wrap = (box.min(2)*2).inject(&:+)
+  bow = box.inject(&:*)
+  wrap + bow
+}.inject(&:+)
+
+puts "Total paper: #{total_sq_paper}"
+puts "Total ribbon: #{total_ribbon}"
